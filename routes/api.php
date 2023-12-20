08b6e6ba100d4a4e26b896891163d5ad644bf7d7
SametKadem <sametkadem@yahoo.com>
2023-12-20 16:36:11 +0300

    first commit

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::middleware('map:token')->get('/test', '\App\Http\Controllers\GeneralController@getToken');
